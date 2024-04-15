4eafb2d866ee00d10e9d9116bfa83abcb644d187
ItsMeSamey <sameychain5041@gmail.com>
2024-04-15 19:02:07 +0530

    5

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
+import viteCompression from 'vite-plugin-compression';
 import solidPlugin from 'vite-plugin-solid';
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
   plugins: [
+    viteCompression({algorithm: 'brotliCompress'}),
     /* 
     Uncomment the following line to enable solid-devtools.
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
