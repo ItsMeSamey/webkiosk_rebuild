89fcf7e0102637254353cbb7c2ac9ddb28dbe049
ItsMeSamey <sameychain5041@gmail.com>
2024-04-15 14:38:32 +0530

    5

---
+++ src/helpers/state.jsx
@@ -0,0 +1,11 @@
+'use strict';
+
+import { createSignal } from "solid-js";
+
+// const anim = a => <Motion.div class="z-0" animate={{opacity: [0, 1]}} transition={{duration: 1, easing: "ease-in-out"}}>{a}</Motion.div>
+
+const [nav, navigate] = createSignal('home');
+const [bar, sidebar] = createSignal(true);
+
+export { nav, navigate, bar, sidebar };
+
